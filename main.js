6b02538404ded38abfe22db23d7be9ce0e2bc2c5
Brydon Holsey <brydon.holsey@ufl.edu>
2020-02-16 20:08:17 -0500

    add functions for boxes 2 and 3

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var box = document.getElementsByClassName ('box');
+var box = document.getElementsByTagName('p');
 
 function bgColor() {
-    document.box[i].style.backgroundColor = 'blue';
+    document.box.style.backgroundColor = 'blue';
 };
 
 for (i = 0; i < box.length; i++) {
@@ -14,9 +14,18 @@ for (i = 0; i < box.length; i++) {
 
 var elementOne = document.createElement('p');
 elementOne = 'oooh - so close, but no cigar';
+var elementTwo = document.createElement('p');
+elementTwo = 'we have a winner!';
+var elementThree = document.createElement('p');
+elementThree = 'oops, better luck next time!';
 
-function onClickOne() {
+function onClick() {
     document.box[0].appendChild('elementOne');
+    document.box[0].appendChild('elementTwo');
+    document.box[0].appendChild('elementThree');
 };
 
-document.box.addEventListener('click', onClickOne);
+document.box[0].addEventListener('click', onClick);
+document.box[1].addEventListener('click', onClick);
+document.box[2].addEventListener('click', onClick);
+
